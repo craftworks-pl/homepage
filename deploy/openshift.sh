febddb3d1652ea39467d029effebddba3d26928a
bgawel <bartek.gawel@gmail.com>
2014-04-24 17:07:35 +0200

    Openshift.sh builds prod version before deployment

--- deploy/openshift.sh
+++ deploy/openshift.sh
@@ -1,9 +1,12 @@
+cd ..
+grunt build
+cd deploy
 rm -rf homepage/*
 cp -r ../dist/* homepage/
 cd homepage
 mv index.html index.php
 git add --all :/
-read -p "Commit message: " message
+read -p "Enter commit/deployment message: " message
 git commit -m message
 git push
 
